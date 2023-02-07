39aad011e846e95dd0f1c86982c4af74f227681e
Vyacheslav <100033730+6hAkunAmAtAtA9@users.noreply.github.com>
2023-02-07 22:34:31 +0300

    Update Fractals_rect.py

--- Fractals_rect.py
+++ Fractals_rect.py
@@ -3,21 +3,20 @@ import time
 import random
 import sys
 
-HEIGHT = 1000
-WEIGHT = 800
+HEIGHT = 800
+WEIGHT = 1000
 BG_COLOR = 'black'
-
 
 sc = pygame.display.set_mode((WEIGHT, HEIGHT))
 sc.fill(BG_COLOR)
 
 next_dot = None
-dots = [(100, 250), (900, 700), (500, 700)]
-
+dots = [(50, 50), (WEIGHT-50, 50), (WEIGHT // 2, HEIGHT-50)]
 
 def dot_in_center(dot1, dot2):
     new_dot = ((dot1[0] + dot2[0]) / 2, (dot1[1] + dot2[1]) / 2)
     return new_dot
+
 
 for dot in dots:
     pygame.draw.circle(sc, 'lime', dot, 5, 2)
